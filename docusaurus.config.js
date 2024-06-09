ed9fdbffc143fa2e1e3ed45056c90fca08a9bca0
gusnaikhsan <gusna.ikhsan7@gmail.com>
2024-06-09 16:28:22 +0700

    fix:oragnization and project name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,8 +20,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "gusna-tech-lab", // Usually your GitHub org/user name.
+  projectName: "gusna-tech-lab-docu", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
