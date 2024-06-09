1f3f6cd7d1274ae2144e8fb79b9742e36e501841
gusnaikhsan <gusna.ikhsan7@gmail.com>
2024-06-09 16:59:06 +0700

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Gusna Tech Lab.",
-  tagline: "Berbagi informasi tekait dunia teknologi",
+  tagline: "Berbagi informasi tekait dunia teknologi.",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
